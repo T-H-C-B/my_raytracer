ef466e09ef6ff5d454e8b8f82e70d4e898b91ca9
Harry Viennot <harry.viennot@epitech.eu>
2023-04-26 13:33:01 +0200

    refactor(Image.hpp): change save method to return pixels instead of saving to file

--- sources/Shared/Utils/Image/Image.hpp
+++ sources/Shared/Utils/Image/Image.hpp
@@ -11,21 +11,21 @@
 #include <string>
 #include <vector>
 #include "Vec3.hpp"
+#include "Raytracer/Core/Scene.hpp"
 
 namespace RayTracer {
     namespace Shared {
         class Image {
         public:
             Image(int width, int height);
-
             void render(Raytracer::Core::Scene& scene);
-            void save(const std::string& filename) const;
+            std::vector<std::vector<Vec3>> getPixels() const { return pixels; };
 
         private:
             void setPixel(int x, int y, const Vec3& color);
             int width;
             int height;
-            std::vector<std::vector<Vec3> pixels>;
+            std::vector<std::vector<Vec3>> pixels;
         };
     }
 }
