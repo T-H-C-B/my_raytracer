ca03518f482bc23c820b6d3c539054d9ade88473
Harry Viennot <harry.viennot@epitech.eu>
2023-04-26 13:31:50 +0200

    add IGraphModule interface for graph modules in RayTracer project

---
+++ sources/Plugins/GraphModule/IGraphModule.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** raytracer
+** File description:
+** IGraphModule
+*/
+
+#ifndef IGRAPHMODULE_HPP_
+#define IGRAPHMODULE_HPP_
+
+#include "ISceneManager.hpp"
+
+namespace RayTracer {
+    namespace Plugins {
+        class IGraphModule {
+            public:
+                IGraphModule();
+                ~IGraphModule();
+
+                virtual void update(RayTracer::Core::IEventManager &eventManager,
+                    RayTracer::Shared::Image &image) = 0;
+            protected:
+            private:
+        };
+    } // namespace Plugins
+} // namespace RayTracer
+
+#endif /* !IGRAPHMODULE_HPP_ */
