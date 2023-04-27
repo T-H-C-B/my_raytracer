849364c38c5deac6221a3fed974fa7b398504c34
Bartoszkk <Bartosz.michalak@epitech.eu>
2023-04-27 14:15:04 +0200

    add of skyboxes and material.cpp

---
+++ .build/sources/Shared/CMakeFiles/raytracer_lib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../../libraytracer_lib.a"
+  "../../../libraytracer_lib.pdb"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Ray/Ray.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Ray/Ray.cpp.o.d"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Vec2/Vec2.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Vec2/Vec2.cpp.o.d"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Vec3/Vec3.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/Geometry/Vec3/Vec3.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
