c898fb2b8cdcfa09a32be9f74bffebdfd748af8c
Harry Viennot <harry.viennot@epitech.eu>
2023-04-30 14:59:37 +0200

    refactor(Cylinder.cpp): simplify cylinder intersection calculation and add support for negative t values feat(Cylinder.cpp): add support for cylinder intersection normal calculation

--- sources/Plugins/Primitives/Cylinders/Cylinder.cpp
+++ sources/Plugins/Primitives/Cylinders/Cylinder.cpp
@@ -21,29 +21,37 @@ void RayTracer::Plugins::Primitives::Cylinder::scale(float scale) {
 
 std::optional<std::unique_ptr<RayTracer::Shared::Intersection>> RayTracer::Plugins::Primitives::Cylinder::intersect(const RayTracer::Shared::Ray& ray, float& t) const
 {
-    RayTracer::Shared::Vec3 oc = ray.getOrigin() - _position;
-    RayTracer::Shared::Vec3 direction = ray.getDirection();
+    float a = ray.getDirection().x * ray.getDirection().x + ray.getDirection().z * ray.getDirection().z;
+    float b = 2 * (ray.getDirection().x * (ray.getOrigin().x - _position.x) + ray.getDirection().z * (ray.getOrigin().z - _position.z));
+    float c = (ray.getOrigin().x - _position.x) * (ray.getOrigin().x - _position.x) + (ray.getOrigin().z - _position.z) * (ray.getOrigin().z - _position.z) - _radius * _radius;
 
-    float a = direction.x * direction.x + direction.z * direction.z;
-    float b = 2.0f * (oc.x * direction.x + oc.z * direction.z);
-    float c = oc.x * oc.x + oc.z * oc.z - _radius * _radius;
     float discriminant = b * b - 4 * a * c;
 
     if (discriminant < 0) {
         return std::nullopt;
     }
 
-    float temp = (-b - sqrt(discriminant)) / (2.0f * a);
-    if (temp < t) {
-        t = temp;
-        return std::nullopt;
+    float t0 = (-b - sqrt(discriminant)) / (2 * a);
+    float t1 = (-b + sqrt(discriminant)) / (2 * a);
+
+    if (t0 > t1) std::swap(t0, t1);
+
+    if (t0 < 1e-4) {
+        t0 = t1;
+        if (t0 < 1e-4) 
+          return std::nullopt;
     }
 
-    temp = (-b + sqrt(discriminant)) / (2.0f * a);
-    if (temp < t) {
-        t = temp;
-        return std::nullopt;
-    }
+    t = t0;
+    RayTracer::Shared::Vec3 hitPoint = ray.pointAt(t);
+    RayTracer::Shared::Vec3 normal = (hitPoint - _position) / _radius;
+    normal.y = 0;
 
-    return std::nullopt;
+    auto intersection = std::make_unique<RayTracer::Shared::Intersection>();
+    intersection->hit = true;
+    intersection->t = t;
+    intersection->point = hitPoint;
+    intersection->normal = normal;
+
+    return std::optional<std::unique_ptr<RayTracer::Shared::Intersection>>(std::move(intersection));
 }
