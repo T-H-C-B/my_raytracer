93dcb5b9dd9ae1a37d8f38b07d65ca2f1a108073
Bartoszkk <Bartosz.michalak@epitech.eu>
2023-04-30 16:15:21 +0200

    fix clement rale

--- sources/Plugins/SkyBox/BlueSkyBox/BlueSkyBox.cpp
+++ sources/Plugins/SkyBox/BlueSkyBox/BlueSkyBox.cpp
@@ -5,12 +5,8 @@
 
 #include "BlueSkyBox.hpp"
 
-namespace RayTracer {
-    namespace Plugins {
-        namespace Skyboxes {
-            RayTracer::Shared::Vec3 BlueSkyBox::getColor(const RayTracer::Shared::Ray &ray) const {
-                return RayTracer::Shared::Vec3(0.0f, 0.0f, 1.0f);
-            }
-        }
-    } // RayTracer
-} // Plugins
+namespace RayTracer::Plugins::Skyboxes {
+    RayTracer::Shared::Vec3 BlueSkyBox::getColor(const RayTracer::Shared::Ray &ray) const {
+        return RayTracer::Shared::Vec3(0.0f, 0.0f, 1.0f);
+    }
+}
