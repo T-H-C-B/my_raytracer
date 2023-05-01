dc9134c5329589b96ec7d077859f7c0b2560bcb4
Bartoszkk <Bartosz.michalak@epitech.eu>
2023-05-01 21:50:27 +0200

    fix

--- sources/Plugins/SkyBox/PNGSkyBox/PNGSkyBoxEntryPoint.cpp
+++ sources/Plugins/SkyBox/PNGSkyBox/PNGSkyBoxEntryPoint.cpp
@@ -5,11 +5,14 @@
 #include "PNGSkyBox.hpp"
 #include "libconfig.h++"
 #include "PluginType.hpp"
+#include "ConfigError.hpp"
 
 extern "C" {
     RayTracer::Plugins::Skyboxes::ISkyBox* create(const libconfig::Setting &setting) {
         std::string path;
         setting.lookupValue("path", path);
+        if (path.empty())
+            throw RayTracer::Shared::ConfigError("PNGSkyBox", "Missing path");
         return new RayTracer::Plugins::Skyboxes::PNGSkyBox(path);
     }
 
