b57429b2609e789b1653529b280f5bda6513cfc5
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2023-05-04 11:41:01 +0200

    Fixed materiel

--- sources/Shared/Materials/Material.cpp
+++ sources/Shared/Materials/Material.cpp
@@ -1,8 +1,7 @@
-//
-// Created by Clément Lagasse on 24/04/2023.
-//
-
 #include "Material.hpp"
+#include "ALight.hpp"
+#include "IEntity.hpp"
+#include "APrimitive.hpp"
 
 namespace RayTracer {
     namespace Shared {
@@ -17,7 +16,35 @@ namespace RayTracer {
             for (auto &decorator : _decorators) {
                 decorator->computeColor(intersection, ray, color, entities);
             }
-            return color;
+
+            std::vector<RayTracer::Plugins::Lights::ALight *> lights;
+            for (auto &lightEntity : entities[Core::EntityType::Light]) {
+                lights.push_back(static_cast<RayTracer::Plugins::Lights::ALight *>(lightEntity));
+            }
+
+            std::vector<RayTracer::Plugins::Primitives::APrimitive *> primitives;
+            for (auto &primitiveEntity : entities[Core::EntityType::Primitive]) {
+                primitives.push_back(static_cast<RayTracer::Plugins::Primitives::APrimitive *>(primitiveEntity));
+            }
+
+            float shadowFactor = 1.0f;
+            for (auto &light : lights) {
+                if (light->inView(intersection.point)) {
+                    Ray shadowRay(intersection.point, (light->getPosition() - intersection.point).normalize());
+                    bool isShadowed = false;
+                    for (auto &primitive : primitives) {
+                        float t;
+                        auto shadowIntersectionOpt = primitive->intersect(shadowRay, t);
+                        if (shadowIntersectionOpt.has_value()) {
+                            isShadowed = true;
+                            break;
+                        }
+                    }
+                    if (isShadowed) shadowFactor -= 1.0f / float(lights.size());
+                }
+            }
+
+            return color * shadowFactor;
         }
-    } // RayTracer
-} // Shared
+    }
+}
