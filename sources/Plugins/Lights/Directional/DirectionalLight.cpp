4f86ac67961979011a1634b0862ba85969c8590c
Clément Lagasse <clement.lagasse@epitech.eu>
2023-05-05 11:04:25 +0200

    Fix Directional light

--- sources/Plugins/Lights/Directional/DirectionalLight.cpp
+++ sources/Plugins/Lights/Directional/DirectionalLight.cpp
@@ -2,6 +2,7 @@
 // Created by Clément Lagasse on 02/05/2023.
 //
 
+#include <cmath>
 #include "DirectionalLight.hpp"
 
 RayTracer::Plugins::Lights::DirectionalLight::DirectionalLight(const RayTracer::Shared::Vec3 &position, const RayTracer::Shared::Vec3 &rotation, float intensity, const RayTracer::Shared::Vec3 &color)
@@ -11,7 +12,16 @@ RayTracer::Plugins::Lights::DirectionalLight::DirectionalLight(const RayTracer::
 
 bool RayTracer::Plugins::Lights::DirectionalLight::inView(const RayTracer::Shared::Vec3 &point) const
 {
-    double NdotL = _direction.dot(point);
+    RayTracer::Shared::Vec3 dist = point - _position;
 
-    return NdotL > 0.0;
+    if (_direction.x != 0 && _direction.y != 0 && _direction.z != 0) {
+        float ratio1 = dist.x / _direction.x;
+        float ratio2 = dist.y / _direction.y;
+        float ratio3 = dist.z / _direction.z;
+
+        return fabs(ratio1 - ratio2) < 1e-6 && fabs(ratio2 - ratio3) < 1e-6;
+    } 
+    else {
+        return false;
+    }
 }
