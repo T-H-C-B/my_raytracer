ac031edfaa8904aaf87311eb9e3148cbe62a7191
Harry Viennot <harry.viennot@epitech.eu>
2023-05-09 23:07:13 +0200

    fixed merge

--- sources/Plugins/Primitives/Cylinders/CylinderEntryPoint.cpp
+++ sources/Plugins/Primitives/Cylinders/CylinderEntryPoint.cpp
@@ -21,13 +21,13 @@ extern "C" {
         RayTracer::Shared::Vec3 color;
 
         if (setting.exists("position")){
-            libconfig::Setting &settingA = setting.lookup("position");
+            const RayTracer::Shared::SettingWrapper &settingA = setting.lookup<RayTracer::Shared::SettingWrapper>("position");
             if (settingA.exists("x") && settingA.exists("y") && settingA.exists("z")) {
                 int x, y, z;
                 try {
-                    x = static_cast<int>(settingA.lookup("x"));
-                    y = static_cast<int>(settingA.lookup("y"));
-                    z = static_cast<int>(settingA.lookup("z"));
+                    x = static_cast<int>(settingA.lookup<int>("x"));
+                    y = static_cast<int>(settingA.lookup<int>("y"));
+                    z = static_cast<int>(settingA.lookup<int>("z"));
                 } catch (const libconfig::SettingTypeException& ex) {
                     throw;
                 }
@@ -37,7 +37,7 @@ extern "C" {
             }
             if (settingA.exists("radius")) {
                 try {
-                    radius = static_cast<float>(settingA.lookup("radius"));
+                    radius = static_cast<float>(settingA.lookup<float>("radius"));
                 } catch (const libconfig::SettingTypeException& ex) {
                     std::cerr << "Error: " << ex.what() << " at " << ex.getPath() << std::endl;
                     throw;
@@ -47,18 +47,19 @@ extern "C" {
             }
         }
         if (setting.exists("rotation")) {
-            libconfig::Setting &settingB = setting.lookup("rotation");
+           const RayTracer::Shared::SettingWrapper &settingB = setting.lookup<RayTracer::Shared::SettingWrapper>("rotation");
             if (settingB.exists("x") && settingB.exists("y") && settingB.exists("z")) {
                 int x, y, z;
                 try {
-                    x = static_cast<int>(settingB.lookup("x"));
-                    y = static_cast<int>(settingB.lookup("y"));
-                    z = static_cast<int>(settingB.lookup("z"));
+                    x = static_cast<int>(settingB.lookup<int>("x"));
+                    y = static_cast<int>(settingB.lookup<int>("y"));
+                    z = static_cast<int>(settingB.lookup<int>("z"));
                 } catch (const libconfig::SettingTypeException& ex) {
                     std::cout << "Test" << std::endl;
                     std::cerr << "Error: " << ex.what() << " at " << ex.getPath() << std::endl;
                     throw;
                 }
+                rotation = RayTracer::Shared::Vec3(x, y, z);
             }
         }
         if (setting.exists("radius")) {
@@ -68,7 +69,6 @@ extern "C" {
                 std::cerr << "Error: " << ex.what() << " at " << ex.getPath() << std::endl;
                 throw;
             }
-            rotation = RayTracer::Shared::Vec3(x, y, z);
         } else {
             throw RayTracer::Shared::ConfigError("Cylinder", "Missing rotation coordinates");
         }
@@ -82,7 +82,6 @@ extern "C" {
     const char *getName() {
         return "Cylinder";
     }
-
 
     RayTracer::Plugins::PluginType getType() {
         return RayTracer::Plugins::PluginType::Entity;
