7ff79c02cc2f4ea2d09d9d15ecd5b5b231b9613e
Clément Lagasse <clement.lagasse@epitech.eu>
2023-05-11 17:52:04 +0200

    Fix Warnings

--- sources/Core/Core.cpp
+++ sources/Core/Core.cpp
@@ -25,9 +25,6 @@ RayTracer::Core::Core::Core(const std::string &graphModuleName, const std::strin
     } catch (const RayTracer::Shared::SettingWrapper::NotFoundException &e) {
         std::cerr << e.what() << std::endl;
         _catchErrors = true;
-    } catch (const RayTracer::Shared::CustomError &e) {
-        std::cerr << e.what() << std::endl;
-        _catchErrors = true;
     }
     try {
         setGraphModule(_graphModuleFactory.create(graphModuleName, root));
@@ -296,11 +293,6 @@ void RayTracer::Core::Core::goNextScene()
         std::unique_ptr<Scene> &new_scene = _sceneManager.getCurrentScene();
         new_scene->init(_pluginLoader.getFactories(), _pluginLoader.getLibraries());
         _imageUpdated = true;
-    } catch (const RayTracer::Shared::CustomError &e) {
-        std::cerr << e.what() << std::endl;
-        _catchErrors = true;
-        _sceneManager.setPreviousScene();
-        return;
     }  catch (const RayTracer::Shared::ConfigError &e) {
         std::cerr << e.what() << std::endl;
         _catchErrors = true;
@@ -331,11 +323,6 @@ void RayTracer::Core::Core::goPreviousScene()
         new_scene->init(_pluginLoader.getFactories(), _pluginLoader.getLibraries());
         _imageUpdated = true;
     }  catch (const RayTracer::Shared::CustomError &e) {
-        std::cerr << e.what() << std::endl;
-        _catchErrors = true;
-        _sceneManager.setPreviousScene();
-        return;
-    }  catch (const RayTracer::Shared::ConfigError &e) {
         std::cerr << e.what() << std::endl;
         _catchErrors = true;
         _sceneManager.setPreviousScene();
