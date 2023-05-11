5efdbf1e7a0b7d4dc079280b2dc9dc10a2495f30
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2023-05-11 10:47:16 +0200

    Fix tests

--- sources/Core/SceneManager.cpp
+++ sources/Core/SceneManager.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <filesystem>
+#include <algorithm>
 #include <iostream>
 #include "SceneManager.hpp"
 #include "CustomError.hpp"
@@ -13,17 +14,25 @@ namespace RayTracer {
     namespace Core {
         SceneManager::SceneManager(const std::string &directory) {
             _currentScene = 0;
+            std::vector<std::string> filepaths;
+
             for (const auto &entry : fs::directory_iterator(directory)) {
                 try {
                     if (entry.is_regular_file() && entry.path().extension() == ".cfg") {
                         std::string filePath = entry.path().string();
-                        _scenes.push_back(std::make_unique<RayTracer::Core::Scene>(filePath));
+                        filepaths.push_back(filePath);
                     }
                 } catch (const fs::filesystem_error &e) {
                     std::cerr << "Error processing scene at " << entry.path() << ": " << e.what() << std::endl;
                 }
             }
+            std::sort(filepaths.begin(), filepaths.end());
+
+            for (const auto &filePath : filepaths) {
+                _scenes.push_back(std::make_unique<RayTracer::Core::Scene>(filePath));
+            }
         }
+
 
         std::unique_ptr<RayTracer::Core::Scene> &SceneManager::getCurrentScene() {
             if (_scenes.empty()) {
